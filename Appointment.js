54c21b22061491fed288684eafb98274f18903f6
PoonamAgg <agarwalit09>
2023-10-02 07:22:43 +0530

    Object in the local storage

--- Appointment.js
+++ Appointment.js
@@ -1,9 +1,27 @@
 function myfunct(event)
 {
     event.preventDefault();
+    const name = event.target.fname.value;
+   const email = event.target.email.value;
+    const phone = event.target.pnumber.value;
+    const date = event.target.date.value;
+    const time = event.target.tcall.value;
 
-    
-var name = document.getElementById("fname").value;
+    const details = {
+        name,
+        email,
+        phone,
+        date,
+        time,
+    };
+  let myObj = JSON.stringify(details)
+  localStorage.setItem('Details', myObj);
+  let myObj1 = JSON.parse(localStorage.getItem("Details"));
+  console.log(myObj1);
+  //showUserOnScreen(myObj);
+
+
+/*var name = document.getElementById("fname").value;
 var email = document.getElementById("e-mail").value;
 var phone = document.getElementById("pnumber").value;
 var date = document.getElementById("date").value;
@@ -21,6 +39,6 @@ localStorage.setItem('is_full_Name', name);
 localStorage.setItem('is_valid_Email', email);
 localStorage.setItem('is_valid_Number', phone);
 localStorage.setItem('Date', date);
-localStorage.setItem('Time_for_call', time);
+localStorage.setItem('Time_for_call', time); */
 
 }
