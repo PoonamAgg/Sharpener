98b9980e2b8ce8c5ab695395b20e9401100d2315
PoonamAgg <agarwalit09>
2023-10-05 05:55:27 +0530

    Edit Button Functionality

--- Appointment.js
+++ Appointment.js
@@ -23,6 +23,7 @@ function myfunct(event)
    {
     const childEle = document.createElement('li');
     const deleteB = document.createElement('button')
+    const EditB = document.createElement('button')
     childEle.textContent= details.name + ' - ' + 
                           details.email + ' - ' + 
                           details.phone + ' - ' + 
@@ -31,14 +32,23 @@ function myfunct(event)
     deleteB.className="button del";
     deleteB.id = `${details.email}`;
     deleteB.textContent="Delete";
+    EditB.className="button edi";
+    EditB.id=`${details.email}`;
+    EditB.textContent="EDIT";
     parentEle.appendChild(childEle);
     parentEle.appendChild(deleteB);
+    parentEle.appendChild(EditB);
    }
   
     const deleteB = document.querySelector('.del')
     parentEle.addEventListener('click', DeleteRow);
     console.log(parentEle)
   
+    const EditB = document.querySelector('.edi');
+    parentEle.addEventListener('click', EditData);
+    console.log(EditB);
+
+    //Delete Function Call
     function DeleteRow(e){
     e.preventDefault();
     const btnId = e.target.id;
@@ -51,6 +61,27 @@ function myfunct(event)
         localStorage.removeItem(btnId);
     }
    } 
+
+   //Edit Function call
+   function EditData(e)
+   {
+    e.preventDefault();
+    const editbtn = e.target.id;
+    if(e.target.classList.contains('edi'))
+    {
+        console.log("Edit Called")
+        localStorage.removeItem(`${deleteB}`);
+
+        //Regain the details
+        const editDetails = document.querySelector('input');
+        editDetails.value=`${editbtn.fname}`;
+        const editSelect =document.querySelectorAll('select');
+        editSelect[0].value=`${editbtn.fname}`;
+
+        e.target.parentEle.remove()
+    }
+    
+   }
  /* let myObj = JSON.stringify(details)
   localStorage.setItem('Details', myObj);
   let myObj1 = JSON.parse(localStorage.getItem("Details"));
