f502dc8f8b93c6699d6d58ad882fa1c227c6bb39
PoonamAgg <agarwalit09>
2023-11-27 13:40:16 +0530

    React-first-project

---
+++ React Projects/my-first-app/src/App.js
@@ -0,0 +1,17 @@
+
+import ExpenseItem from './components/ExpenseItem';
+
+
+function App(){
+   
+
+    return(
+        <div>
+            <h2> Expense Tracker</h2>
+            <ExpenseItem >
+            </ExpenseItem>
+        </div>
+);
+}
+
+export default App;
