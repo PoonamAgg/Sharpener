8e9b99da250a1f3a8a8d9ef27748431d90ddf02e
PoonamAgg <agarwalit09>
2023-11-29 14:51:04 +0530

    Map changes

--- React Projects/my-first-app/src/App.js
+++ React Projects/my-first-app/src/App.js
@@ -1,18 +1,43 @@
+import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses";
 
-import ExpenseItem from './components/ExpenseItem';
+function App() {
+  let expenses = [
+    {
+      id: "e1",
+      title: "School fee",
+      amount: 250,
+      date: new Date(2021, 5, 12),
+    },
+    {
+      id: "e2",
+      title: "Books",
+      amount: 350,
+      date: new Date(2021, 3, 12),
+    },
+    {
+      id: "e3",
+      title: "house rent",
+      amount: 4250,
+      date: new Date(2021, 5, 11),
+    },
+    {
+      id: "e4",
+      title: "food",
+      amount: 550,
+      date: new Date(2023, 9, 12),
+    },
+  ];
 
+  return (
+    <div>
+      <h2> Expense Tracker</h2>
 
-function App(){
-    
-
-    return(
-        <div>
-            <h2> Expense Tracker</h2>
-            <ExpenseItem >
-                
-            </ExpenseItem>
-        </div>
-);
+      {expenses.map((expense) => (
+        <ExpenseItem item={expense} />
+      ))}
+    </div>
+  );
 }
 
 export default App;
