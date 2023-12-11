ef9f5d0095bcfcce94e531d9ac9497d8c456782d
PoonamAgg <agarwalit09>
2023-12-11 18:37:47 +0530

    DIY  Challenges on Conditional Rendering

--- React Projects/my-first-app/src/components/NewExpense/ExpenseForm.js
+++ React Projects/my-first-app/src/components/NewExpense/ExpenseForm.js
@@ -48,7 +48,9 @@ const ExpenseForm = (props) => {
           <input type="date" value ={enteredDate} min="2019-01-01" max="2022-12-31" onChange={dateChangeHandler} />
         </div>
       </div>
+      
       <div className="new-expense__actions">
+      <button type="button" onClick={props.onCancel}>Cancel</button>
         <button type="submit">Add Expense</button>
       </div>
     </form>
